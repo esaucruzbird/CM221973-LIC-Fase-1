8af260f68919e4c20d71d1aa983ada556587d077
esaucruz <esaucruzbird@gmail.com>
2024-05-08 22:31:59 -0600

    alerta para salir del sistema en Deposito

--- menu/js/autoria/logica/logicaDeposito.js
+++ menu/js/autoria/logica/logicaDeposito.js
@@ -63,8 +63,31 @@ document.getElementById('btnRegresar').addEventListener('click', function () {
   window.location.href = '../menu.html'; //redirige a la página de depósito
   //código de JavaScript a ejecutar cuando se abra la página web seleccionada
 });
+
+document.getElementById('btnCerrar').addEventListener('click', function () {
+  // Mostrar ventana emergente de Sweet Alert
+  Swal.fire({
+      title: '¿Estás seguro?',
+      text: '¿Deseas salir del sitio?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Sí, salir',
+      cancelButtonText: 'No, quedarse'
+  }).then((result) => {
+      if (result.isConfirmed) {
+          // Si el usuario elige "Sí, salir", redirigirlo a la página index para loguearse de nuevo
+          window.location.href = '../../index.html'; //redirige a la página de logueo
+          //código de JavaScript a ejecutar cuando se abra la página web seleccionada
+      }
+  });
+});
+
+/*
 document.getElementById('btnCerrar').addEventListener('click', function () {
   window.location.href = '../../index.html'; //redirige a la página de depósito
   //código de JavaScript a ejecutar cuando se abra la página web seleccionada
 });
+*/
 //FIN Lógica para llamamiento de los botones
