e813564e2171da307418c0b0a4a9f4ca479d44ac
esaucruz <esaucruzbird@gmail.com>
2024-05-08 21:07:28 -0600

    Incorporando logica y validacion a Retiro

---
+++ menu/js/autoria/logica/logicaRetiro.js
@@ -0,0 +1,68 @@
+//INICIO función realizarDeposito
+function realizarRetiro() {
+  var cantidadRetirar = document.getElementById('cantidadRetirar').value;
+
+  // Validar la cantidad a depositar
+  if (!validarCantidadRetirar(cantidadRetirar)) {
+    return;
+  }
+
+  // Aquí continúa la lógica para página depósito
+  // Obtener la cantidad a retirar desde el input
+  var cantidad = parseFloat(document.getElementById('cantidadRetirar').value);
+  // Validar que la cantidad sea un número válido
+  if (isNaN(cantidad) || cantidad <= 0) {
+    alert('Por favor ingresa una cantidad válida.'); //alerta que se lanza si en el campo está vacio, cero o negativo
+    return;
+  }
+  // Obtener el saldo actual del localStorage y restarle la cantidad a retirar
+  var saldoActual = parseFloat(localStorage.getItem('saldo'));
+  var nuevoSaldo = saldoActual - cantidad;
+  // Actualizar el saldo en el localStorage
+  localStorage.setItem('saldo', nuevoSaldo.toString());
+  // Mostrar un mensaje de confirmación
+  alert('Se ha retirado $' + cantidad + ' . Nuevo saldo: $' + nuevoSaldo + ' .');
+  document.getElementById('cantidadRetirar').value = '';
+  //Almacenando transacción en el historial
+  var nuevaTransaccion = {
+    concepto: 'retiro',
+    fecha: obtenerFechaActual(), // Función para obtener la fecha actual
+    valor: cantidad,
+  };
+  var historial = JSON.parse(localStorage.getItem('historial')) || [];
+  historial.push(nuevaTransaccion);
+  localStorage.setItem('historial', JSON.stringify(historial));
+
+  //PARA QUE SE ACTUALICE EL SALDO EN EL ENCABEZADO AL HACER LA TRANSACCIÓN
+  // Obtener el saldo actual del localStorage
+  var saldoActual = parseFloat(localStorage.getItem('saldo'));
+  //asignación del saldo al encabezado
+  document.getElementById('saldo-encabezado').innerHTML = 'El N° cuenta es 0987654321, el saldo actual es $ ' + saldoActual;
+}
+//FIN función realizarDeposito
+
+function obtenerFechaActual() {
+  var fecha = new Date();
+  var dia = fecha.getDate();
+  var mes = fecha.getMonth() + 1; // Se agrega 1 porque los meses comienzan desde 0
+  var año = fecha.getFullYear();
+  // Formatear la fecha en el formato dd-mm-yyyy
+  return (dia < 10 ? '0' : '') + dia + '-' + (mes < 10 ? '0' : '') + mes + '-' + año;
+}
+
+//PARA QUE ESTÉ EN EL ENCABEZADO DESDE QUE SE CARGA LA PÁGINA
+// Obtener el saldo actual del localStorage
+var saldoActual = parseFloat(localStorage.getItem('saldo'));
+//asignación del saldo al encabezado
+document.getElementById('saldo-encabezado').innerHTML = 'El N° cuenta es 0987654321, el saldo actual es $ ' + saldoActual;
+
+//INICIO Lógica para llamamiento de los botones
+document.getElementById('btnRegresar').addEventListener('click', function () {
+  window.location.href = '../menu.html'; //redirige a la página de depósito
+  //código de JavaScript a ejecutar cuando se abra la página web seleccionada
+});
+document.getElementById('btnCerrar').addEventListener('click', function () {
+  window.location.href = '../../index.html'; //redirige a la página de depósito
+  //código de JavaScript a ejecutar cuando se abra la página web seleccionada
+});
+//FIN Lógica para llamamiento de los botones
