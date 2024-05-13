6a3e82870cf58f73be00bf4bdcb4f3fd3f70758f
esaucruz <esaucruzbird@gmail.com>
2024-05-13 12:58:40 -0600

    Incorporando sweetalert a logica y validacion pagoTelefono

--- menu/js/autoria/validacion/validacionPagoTelefono.js
+++ menu/js/autoria/validacion/validacionPagoTelefono.js
@@ -2,47 +2,63 @@
 // con la variable "validate" se asume que ValidateJS ya está disponible globalmente en el navegador
 
 if (typeof validate === 'undefined') {
-    document.write('<script src="https://cdnjs.cloudflare.com/ajax/libs/validate.js/0.13.1/validate.min.js"><\/script>');
+  document.write('<script src="https://cdnjs.cloudflare.com/ajax/libs/validate.js/0.13.1/validate.min.js"></script>');
 }
 
 function validarPagoTelefono(pagoTelefono) {
-    var constraints = {
-        pagoTelefono: {
-        presence:{
-            message: "Este campo no puede quedar vacío"
-        },
-        numericality: {
-          greaterThan: 0,
-          message: "La cantidad a retirar no puede quedar vacía y debe ser mayor que cero"
-        }
-      }
-    };
-  
-    var errors = validate({ pagoTelefono: pagoTelefono }, constraints);
-    if (errors) {
-      alert(errors.pagoTelefono[0]);
-      return false;
-    }
-    return true;
-  }
+  var constraints = {
+    pagoTelefono: {
+      presence: {
+        message: 'Este campo no puede quedar vacío',
+      },
+      numericality: {
+        greaterThan: 0,
+        message: 'La cantidad a retirar no puede quedar vacía y debe ser mayor que cero',
+      },
+    },
+  };
 
-  function validarNpeTelefono(npeTelefono) {
-    var constraints = {
-        npeTelefono: {
-        presence:{
-            message: "Este campo no puede quedar vacío"
-        },
-        length:{
-            is: 24,
-            message: "El NPE debe tener una longitud de 20 caracteres"
-        }
-      }
-    };
-  
-    var errors = validate({ npeTelefono: npeTelefono }, constraints);
-    if (errors) {
-      alert(errors.npeTelefono[0]);
-      return false;
-    }
-    return true;
+  var errors = validate({ pagoTelefono: pagoTelefono }, constraints);
+  if (errors) {
+    // Mostrar el mensaje de error del campo validado
+    Swal.fire({
+      title: 'Error de Validación',
+      text: errors.pagoTelefono[0],
+      // indica la posición (0) del mensaje que se debe mostrar, al romoperse alguna de las 2 reglas que tiene el campo. Como solo se puede romper 1 a la vez. Siempre se debe llamar a la posición 0 de la matriz error contrasena
+      icon: 'error',
+      backdrop: false,
+    });
+    //alert(errors.pagoTelefono[0]);
+    return false;
   }
+  return true;
+}
+
+function validarNpeTelefono(npeTelefono) {
+  var constraints = {
+    npeTelefono: {
+      presence: {
+        message: 'Este campo no puede quedar vacío',
+      },
+      length: {
+        is: 24,
+        message: 'El NPE debe tener una longitud de 20 caracteres',
+      },
+    },
+  };
+
+  var errors = validate({ npeTelefono: npeTelefono }, constraints);
+  if (errors) {
+    // Mostrar el mensaje de error del campo validado
+    Swal.fire({
+      title: 'Error de Validación',
+      text: errors.npeTelefono[0],
+      // indica la posición (0) del mensaje que se debe mostrar, al romoperse alguna de las 2 reglas que tiene el campo. Como solo se puede romper 1 a la vez. Siempre se debe llamar a la posición 0 de la matriz error contrasena
+      icon: 'error',
+      backdrop: false,
+    });
+    //alert(errors.npeTelefono[0]);
+    return false;
+  }
+  return true;
+}
