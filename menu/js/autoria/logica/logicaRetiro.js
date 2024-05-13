832ef164cce568035839675221510d124a90ab33
esaucruz <esaucruzbird@gmail.com>
2024-05-13 00:17:27 -0600

    Incorporar sweetalert en logica y validacion retiro

--- menu/js/autoria/logica/logicaRetiro.js
+++ menu/js/autoria/logica/logicaRetiro.js
@@ -14,7 +14,15 @@ function realizarRetiro() {
   var cantidad = parseFloat(document.getElementById('cantidadRetirar').value);
   // Validar que la cantidad sea un número válido
   if (isNaN(cantidad) || cantidad <= 0) {
-    alert('Por favor ingresa una cantidad válida.'); //alerta que se lanza si en el campo está vacio, cero o negativo
+    //alerta que se lanza si en el campo está vacio, cero o negativo
+    // Mostrar el mensaje de error del campo validado
+    Swal.fire({
+      title: 'Por favor ingresa una cantidad válida.',
+      text: errors.cantidadDepositar[0],
+      // indica la posición (0) del mensaje que se debe mostrar, al romoperse alguna de las 2 reglas que tiene el campo. Como solo se puede romper 1 a la vez. Siempre se debe llamar a la posición 0 de la matriz error contrasena
+      icon: 'error',
+      backdrop: false,
+    });
     return;
   }
   // Obtener el saldo actual del localStorage y restarle la cantidad a retirar
@@ -23,7 +31,15 @@ function realizarRetiro() {
   // Actualizar el saldo en el localStorage
   localStorage.setItem('saldo', nuevoSaldo.toString());
   // Mostrar un mensaje de confirmación
-  alert('Se ha retirado $' + cantidad + ' . Nuevo saldo: $' + nuevoSaldo + ' .');
+  // Mostrar el mensaje de error del campo validado
+  Swal.fire({
+    title: 'Éxito',
+    text: 'Se ha retirado $' + cantidad + ' . Nuevo saldo: $' + nuevoSaldo + ' .',
+    // indica la posición (0) del mensaje que se debe mostrar, al romoperse alguna de las 2 reglas que tiene el campo. Como solo se puede romper 1 a la vez. Siempre se debe llamar a la posición 0 de la matriz error contrasena
+    icon: 'success',
+    backdrop: false,
+  });
+  //alert('Se ha retirado $' + cantidad + ' . Nuevo saldo: $' + nuevoSaldo + ' .');
   document.getElementById('cantidadRetirar').value = '';
   //Almacenando transacción en el historial
   var nuevaTransaccion = {
@@ -65,24 +81,24 @@ document.getElementById('btnRegresar').addEventListener('click', function () {
 });
 
 document.getElementById('btnCerrar').addEventListener('click', function () {
-    // Mostrar ventana emergente de Sweet Alert
-    Swal.fire({
-        title: '¿Estás seguro?',
-        text: '¿Deseas salir del sitio?',
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Sí, salir',
-        cancelButtonText: 'No, quedarse',
-        backdrop: false
-    }).then((result) => {
-        if (result.isConfirmed) {
-            // Si el usuario elige "Sí, salir", redirigirlo a la página index para loguearse de nuevo
-            window.location.href = '../../index.html'; //redirige a la página de logueo
-            //código de JavaScript a ejecutar cuando se abra la página web seleccionada
-        }
-    });
+  // Mostrar ventana emergente de Sweet Alert
+  Swal.fire({
+    title: '¿Estás seguro?',
+    text: '¿Deseas salir del sitio?',
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Sí, salir',
+    cancelButtonText: 'No, quedarse',
+    backdrop: false,
+  }).then((result) => {
+    if (result.isConfirmed) {
+      // Si el usuario elige "Sí, salir", redirigirlo a la página index para loguearse de nuevo
+      window.location.href = '../../index.html'; //redirige a la página de logueo
+      //código de JavaScript a ejecutar cuando se abra la página web seleccionada
+    }
+  });
 });
 
 /*
--- menu/js/autoria/validacion/validacionRetiro.js
+++ menu/js/autoria/validacion/validacionRetiro.js
@@ -2,26 +2,34 @@
 // con la variable "validate" se asume que ValidateJS ya está disponible globalmente en el navegador
 
 if (typeof validate === 'undefined') {
-    document.write('<script src="https://cdnjs.cloudflare.com/ajax/libs/validate.js/0.13.1/validate.min.js"><\/script>');
+  document.write('<script src="https://cdnjs.cloudflare.com/ajax/libs/validate.js/0.13.1/validate.min.js"></script>');
 }
 
 function validarCantidadRetirar(cantidadRetirar) {
-    var constraints = {
-        cantidadRetirar: {
-        presence:{
-            message: "Este campo no puede quedar vacío"
-        },
-        numericality: {
-          greaterThan: 0,
-          message: "La cantidad a retirar no puede quedar vacía y debe ser mayor que cero"
-        }
-      }
-    };
-  
-    var errors = validate({ cantidadRetirar: cantidadRetirar }, constraints);
-    if (errors) {
-      alert(errors.cantidadRetirar[0]);
-      return false;
-    }
-    return true;
+  var constraints = {
+    cantidadRetirar: {
+      presence: {
+        message: 'Este campo no puede quedar vacío',
+      },
+      numericality: {
+        greaterThan: 0,
+        message: 'La cantidad a retirar no puede quedar vacía y debe ser mayor que cero',
+      },
+    },
+  };
+
+  var errors = validate({ cantidadRetirar: cantidadRetirar }, constraints);
+  if (errors) {
+    // Mostrar el mensaje de error del campo validado
+    Swal.fire({
+      title: 'Error de Validación',
+      text: errors.cantidadRetirar[0],
+      // indica la posición (0) del mensaje que se debe mostrar, al romoperse alguna de las 2 reglas que tiene el campo. Como solo se puede romper 1 a la vez. Siempre se debe llamar a la posición 0 de la matriz error contrasena
+      icon: 'error',
+      backdrop: false,
+    });
+    //alert(errors.cantidadRetirar[0]);
+    return false;
   }
+  return true;
+}
